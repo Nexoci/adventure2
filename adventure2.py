ca2f22dcd033f1b70b881a5865e91827d88a5c7a
Nexoci <konnork84@kprschools.ca>
2023-10-17 14:29:19 -0400

    new working

--- adventure2.py
+++ adventure2.py
@@ -11,20 +11,20 @@ fps = 30
 fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 700
 WINDOW_HEIGHT = 800
-move_group = pygame.sprite.Group
-maze_group = pygame.sprite.Group
-font2= pygame.font.SysFont('Arial', 20)
+move_group = pygame.sprite.Group()
+maze_group = pygame.sprite.Group()
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
-henry= Player(40,40,35,35,"images/Smiley.jpg",2)
+font2= pygame.font.SysFont('Arial', 20)
+henry= Player(40,33,25,25,"images/Smiley.jpg",5)
 move_group.add(henry)
 maze= Maze(WINDOW_WIDTH,WINDOW_HEIGHT,"images/maze.png")
 maze_group.add(maze)
+btn_ext=()
 pygame.display.set_caption("Adventure")
 def display():
     window.fill((255,255,255))
-    #move_group.draw(window)
     maze_group.draw(window)
-    #Placing Control Images and Text to help the player
+    move_group.draw(window)
 
     
 while True:
@@ -39,7 +39,10 @@ while True:
             pos = pygame.mouse.get_pos()
             #If they click the reset button the game will reset
     
-    #move_group.move()
+    henry.move()
+    if henry.check_hit(maze_group):
+        henry.back()
+        
        
     pygame.display.update() #update the display
     fpsClock.tick(fps) #speed of redraw
--- player.py
+++ player.py
@@ -9,9 +9,18 @@ class Player(pygame.sprite.Sprite):
             self.mask  = pygame.mask.from_surface(self.image)
             self.rect = self.image.get_rect(topleft=(startX,startY))
             self.speed = speed
+    def back(self):
+        self.rect.x -= self.movex
+        self.rect.y -= self.movey
     def move(self):
         key_input = pygame.key.get_pressed()
-        movex = (key_input[pygame.K_LEFT] * -self.speed) + (key_input[pygame.K_RIGHT] * self.speed)
-        movey = (key_input[pygame.K_UP] * -self.speed) + (key_input[pygame.K_DOWN] * self.speed)
-        playerx += movex
-        playery += movey
+        self.movex = (key_input[pygame.K_LEFT] * -self.speed) + (key_input[pygame.K_RIGHT] * self.speed)
+        self.movey = (key_input[pygame.K_UP] * -self.speed) + (key_input[pygame.K_DOWN] * self.speed)
+        self.rect.x += self.movex
+        self.rect.y += self.movey
+    def check_hit(self,group):
+        if pygame.sprite.spritecollide(self,group, False, collided=pygame.sprite.collide_mask):
+            return True
+        else:
+            return False
+        
